a95c44d2b06c227d4fcf59d0b498294fd3891246
Alperen Demirkol <74583848+alpogant@users.noreply.github.com>
2023-04-20 11:34:57 +0300

    verifying crc corrected

--- lora_rover/src/lorarx.cpp
+++ lora_rover/src/lorarx.cpp
@@ -87,34 +87,26 @@ int main()
             // Extract packet data
             Packet packet;
             memcpy(&packet, buffer, sizeof(Packet));
-
-            // Extract packet data and CRC
-            uint8_t packet_data[4];
-            uint16_t packet_crc = 0;
-            for (int i = 0; i < 6; i++) 
-            {
-                if (i < 4) 
-                {
-                    packet_data[i] = buffer[i];
-                } else if (i < 6) 
-                {
-                    packet_crc |= (uint16_t)buffer[i] << ((i-4)*8);
-                }
-            }
+            
+            packet.value1 = (int8_t)buffer[0];
+            packet.value2 = (int8_t)buffer[1];
+            packet.value3 = (uint8_t) (buffer[2] & 0x0F);
+            packet.value4 = (uint8_t) ((buffer[2] & 0xF0) >> 4);
+            printf("Received Packet: %d %d %d %d\n", packet.value1, packet.value2, packet.value3, packet.value4);
+            
+            packet.crc = ((uint16_t)buffer[3] << 8) | (uint16_t)buffer[4];
+            
+            //calculated crc
+            uint16_t crc = crc16(reinterpret_cast<uint8_t*>(&packet), sizeof(packet) - 3);
 
             // Verify CRC
-            uint16_t crc = crc16(buffer, sizeof(Packet)-3);
             if (crc != packet.crc)
             {
                 std::cerr << "CRC mismatch" << std::endl;
                 continue;
             }
 
-            packet.value1 = (int8_t)buffer[0];
-            packet.value2 = (int8_t)buffer[1];
-            packet.value3 = (uint8_t) (buffer[2] & 0x0F);
-            packet.value4 = (uint8_t) ((buffer[2] & 0xF0) >> 4);
-            printf("Received Packet: %d %d %d %d\n", packet.value1, packet.value2, packet.value3, packet.value4);
+
         }
     }
 }
