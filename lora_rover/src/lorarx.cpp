018ff9c8c16d4eef9ff0f736ba9d15f9c8ecf54e
Alperen Demirkol <74583848+alpogant@users.noreply.github.com>
2023-05-04 19:26:00 +0300

    first fully working rx code
    in printf session "\n" part is very important.

--- lora_rover/src/lorarx.cpp
+++ lora_rover/src/lorarx.cpp
@@ -28,20 +28,20 @@ typedef struct {
 } Packet;
 #pragma pack(pop)
 
-// Function to calculate CRC16
-uint16_t crc16(const uint8_t* data, size_t length)
+// CRC-16 polynomial: x^16 + x^15 + x^2 + 1 (0x8005)
+uint16_t crc16(uint8_t *data, size_t length) 
 {
     uint16_t crc = 0xFFFF;
-    for (size_t i = 0; i < length; ++i)
+    for (size_t i = 0; i < length; i++) 
     {
         crc ^= (uint16_t)data[i] << 8;
-        for (size_t j = 0; j < 8; ++j)
+        for (int j = 0; j < 8; j++) 
         {
-            if (crc & 0x8000)
+            if (crc & 0x8000) 
             {
-                crc = (crc << 1) ^ 0x1021;
-            }
-            else
+                crc = (crc << 1) ^ 0x8005;
+            } 
+            else 
             {
                 crc <<= 1;
             }
@@ -68,7 +68,7 @@ int main()
     tty.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
     tty.c_oflag &= ~OPOST;
     tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 10;
+    tty.c_cc[VTIME] = 0;
     tcsetattr(fd, TCSANOW, &tty);
     fd = open(DEVICE, O_RDWR | O_NOCTTY | O_SYNC);
 
@@ -82,41 +82,47 @@ int main()
     {
         uint8_t buffer[sizeof(Packet)];
         
-        //tcflush(fd, TCIFLUSH);
+        tcflush(fd, TCIFLUSH);
         ssize_t n = read(fd, buffer, sizeof(buffer)); // read a packet
 
-        if (n == sizeof(buffer))
+        printf("%02X", buffer[0]);
+        printf("%02X", buffer[1]);
+        printf("%02X", buffer[2]);
+        printf("%02X", buffer[3]);
+        printf("%02X", buffer[4]);
+        printf("\n");
+
+        // Check the newline character
+        if (buffer[5] != '\n')
         {
-            // Check the newline character
-            if (buffer[sizeof(Packet)-1] != '\n')
-            {
-                std::cerr << "Error: newline character not found" << std::endl;
-                continue;
-            }
+            std::cerr << "Error: newline character not found" << std::endl;
+            continue;
+        }
 
-            // Extract packet data
-            Packet packet;
-            memcpy(&packet, buffer, sizeof(Packet));
-            
-            packet.value1 = (int8_t)buffer[0];
-            packet.value2 = (int8_t)buffer[1];
-            packet.value3 = (uint8_t) (buffer[2] & 0x0F);
-            packet.value4 = (uint8_t) ((buffer[2] & 0xF0) >> 4);
+        // Extract packet data
+        Packet packet;
+        memcpy(&packet, buffer, sizeof(Packet));
+
+        packet.value1 = (int8_t)buffer[0];
+        packet.value2 = (int8_t)buffer[1];
+        packet.value3 = (uint8_t) (buffer[2] & 0x0F);
+        packet.value4 = (uint8_t) ((buffer[2] & 0xF0) >> 4);
+
+        packet.crc = ((uint16_t)buffer[3] << 8) | (uint16_t)buffer[4];
+
+        //calculated crc
+        uint16_t crc = crc16(reinterpret_cast<uint8_t*>(&packet), sizeof(packet) - 3);
+
+        // Verify CRC
+        if (crc != packet.crc)
+        {
+            std::cerr << "CRC mismatch" << std::endl;
+            continue;
+        }
+
+        else 
+        {
             printf("Received Packet: %d %d %d %d\n", packet.value1, packet.value2, packet.value3, packet.value4);
-            
-            packet.crc = ((uint16_t)buffer[3] << 8) | (uint16_t)buffer[4];
-            
-            //calculated crc
-            uint16_t crc = crc16(reinterpret_cast<uint8_t*>(&packet), sizeof(packet) - 3);
-
-            // Verify CRC
-            if (crc != packet.crc)
-            {
-                std::cerr << "CRC mismatch" << std::endl;
-                continue;
-            }
-
-
         }
     }
 }
